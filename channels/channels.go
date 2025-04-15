a99f8489c3e7327bfd464413750bb89703252dec
Marcus <88283829+PyMarcus@users.noreply.github.com>
2025-04-15 13:44:05 -0300

    Update channels.go

--- channels/channels.go
+++ channels/channels.go
@@ -1,3 +1,37 @@
 package channels
 
+import "fmt"
 
+// pong: listen the channel
+func pong(wordSentCh, wordReceivedCh chan string) {
+	for {
+		word := <-wordSentCh
+		fmt.Println("ping: " + word)
+		wordReceivedCh <- word
+	}
+}
+
+func main() {
+	// ping-pong with channels
+	wordSentCh := make(chan string)
+	wordReceivedCh := make(chan string)
+
+	go pong(wordSentCh, wordReceivedCh)
+
+	for {
+		var input string
+		fmt.Print("->: ")
+		_, _ = fmt.Scanln(&input)
+
+		if input == "q" {
+			break
+		}
+
+		wordSentCh <- input
+
+		fmt.Println("pong: " + <-wordReceivedCh)
+	}
+
+	close(wordReceivedCh)
+	close(wordSentCh)
+}
